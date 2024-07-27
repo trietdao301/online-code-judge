9d46b8cade91522e12627cb82efaaedaf1c50bf7
Triet Dao <trietdao301@gmail.com>
2024-07-27 15:54:41 -0800

    .

--- handlers/api_server_handler.go
+++ handlers/api_server_handler.go
@@ -25,7 +25,7 @@ type apiFunc func(http.ResponseWriter, *http.Request) error
 func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		//w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		w.Header().Set("Access-Control-Allow-Origin", "https://online-code-judge-5x96oh9rh-trietdao301s-projects.vercel.app")
+		w.Header().Set("Access-Control-Allow-Origin", "https://online-code-judge-fe.vercel.app")
 
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
